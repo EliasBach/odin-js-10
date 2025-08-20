5b7e5dc2529cc8bf61d58166ac084083f4e5617d
EliasBach <el.bach1999@gmail.com>
2025-08-20 19:48:15 +0200

    code functional but not optimised

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-function ChessKnightMovesToGoal(X, Y, goal_X, goal_Y, visited_coordinates = [[X, Y]]) {
-    // check conditions to end recursion
+const X_start = 2
+const Y_start = 1
+const X_goal = 0
+const Y_goal = 0
+let path = [[X_start, Y_start]]
+ChessKnightMovesToGoal(X_goal, Y_goal, path)
+
+function ChessKnightMovesToGoal(goal_X, goal_Y, path) {
+    let last_move = path.slice(-1)[0]
+    let current_X = last_move[0]
+    let current_Y = last_move[1]
     
-    if (X == goal_X && Y == goal_Y) { 
-        console.log("Path found")
-        for (let XY of visited_coordinates) {
+    // check conditions to end recursion
+    if (current_X == goal_X && current_Y == goal_Y) {
+        console.log("Path found:")
+        for (let XY of path) {
             console.log(XY)
         }
         return
     } 
-    
+
+    if (path.length > 6) {
+        return
+    }
     
     // try one of eight possible moves (update next move and call again)
     // update position and visited moves
-    for (let move of newMovesFrom(X, Y, visited_coordinates)) {
-        visited_coordinates.push(move)
-        X = move[0]
-        Y = move[1]
-        if (visited_coordinates.length > 6) {
-            console.log("Too many moves made.")
-            return
-        }
-
-        ChessKnightMovesToGoal(X, Y, goal_X, goal_Y, visited_coordinates)
+    for (let move of newMovesFrom(current_X, current_Y, path)) {
+        let current_path = path
+        ChessKnightMovesToGoal(goal_X, goal_Y, [...current_path, move])
     }
     return
 }
 
-function newMovesFrom(X, Y, visited_coordinates) {
+function newMovesFrom(X, Y, visited_coordinates = []) {
     const newMoves = []
+    console.log(visited_coordinates)
     if (X+2 <= 7 && Y+1 <= 7) {newMoves.push([X+2, Y+1])}
     if (X+2 <= 7 && Y-1 >= 0) {newMoves.push([X+2, Y-1])}
     if (X+1 <= 7 && Y+2 <= 7) {newMoves.push([X+1, Y+2])}
@@ -39,10 +46,3 @@ function newMovesFrom(X, Y, visited_coordinates) {
     const visitedSet = new Set(visited_coordinates.map(c => `${c[0]},${c[1]}`))
     return newMoves.filter(c => !visitedSet.has(`${c[0]},${c[1]}`))
 }
-
-// testing
-const X = 5
-const Y = 3
-const goal_X = 3
-const goal_Y = 2
-ChessKnightMovesToGoal(X, Y, goal_X, goal_Y)
